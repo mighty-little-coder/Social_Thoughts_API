b168d13271a4b5c6fff43b6cdb74f6b717ec2539
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2024-02-03 01:13:32 -0500

    Added to userController file

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,42 +1,110 @@
+const router = require('express').Router();
+const { Thought, User } = require('../../models');
 
+module.exports = {
+  async getAllUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async getUserById(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+      .populate('friends')
+      .populate('thoughts')
+      .select('-__v');
 
+      if (!user) {
+        return res.status(404).json({ message: 'No user profile matches that ID' });
+      }
 
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
-// EXAMPLE CODE BELOW
+  async updateUserById(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
 
-// const User = require('../models/User');
+      if (!user) {
+        return res.status(404).json({ message: 'Cannot update user because no user profile matches that ID' });
+      }
 
-// module.exports = {
-//   async getUsers(req, res) {
-//     try {
-//       const users = await User.find();
-//       res.json(users);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   },
-//   async getSingleUser(req, res) {
-//     try {
-//       const user = await User.findOne({ _id: req.params.userId })
-//         .select('-__v');
+      res.json(user);
+    }
+    catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
-//       if (!user) {
-//         return res.status(404).json({ message: 'No user with that ID' });
-//       }
+  async deleteUserById(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
 
-//       res.json(user);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   },
-//   // create a new user
-//   async createUser(req, res) {
-//     try {
-//       const dbUserData = await User.create(req.body);
-//       res.json(dbUserData);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   },
-// };
+      if (!user) {
+        return res.status(404).json({ message: 'Cannot delete user because no user profile matches that ID' });
+      }
+
+      res.json(user);
+    }
+    catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async addFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.params.friendId } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: 'Cannot add friend to user because no user profile matches that ID' });
+      }
+
+      res.json(user);
+    }
+    catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async removeFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendId } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: 'Cannot remove friend from user because no user profile matches that ID' });
+      }
+
+      res.json(user);
+    }
+    catch (err) {
+      res.status(500).json(err);
+    }
+  }
+};
