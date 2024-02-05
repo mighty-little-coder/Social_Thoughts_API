fe9490b703c12e4df3599fcba43d139746c01c07
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2024-02-05 02:11:44 -0500

    Fixed thoughtsSchema to include username to attach to a user

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,26 +1,7 @@
 const { Schema, model, default: mongoose } = require('mongoose');
 const moment = require('moment');
 
-
-
-const thoughtsSchema = new Schema(
-  {
-    thoughtText: {
-      type: String,
-      required: 'Text is required',
-      minlength: 1,
-      maxlength: 280,
-    },
-
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      // Add adjustable timestamp based on how long ago post was made (one second ago, ${minutes} ago, ${hours} ago, etc)
-      get: thoughtTimestampFormat => moment(thoughtTimestampFormat).format("MMM DD, YYYY [at] hh:mm a")
-    },
-  },
-);
-
+// Create a reaction schema
 const reactionSchema = new Schema(
   {
     reactionId: {
@@ -57,6 +38,33 @@ const reactionSchema = new Schema(
   }
 );
 
+// Create a thoughts schema
+const thoughtsSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: 'Text is required',
+      minlength: 1,
+      maxlength: 280,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // Add adjustable timestamp based on how long ago post was made (one second ago, ${minutes} ago, ${hours} ago, etc)
+      get: thoughtTimestampFormat => moment(thoughtTimestampFormat).format("MMM DD, YYYY [at] hh:mm a")
+    },
+
+    username: {
+      type: String,
+      required: 'Username is required',
+    },
+
+    reactions: [reactionSchema]
+  },
+);
+
+// Create a total count of reactions to a thought
 thoughtsSchema.virtual('reactionTally').get(function () {
   return this.reactions.length;
 });
